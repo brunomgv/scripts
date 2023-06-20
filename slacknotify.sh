966689893b1edc31a94af480b872012855aeffa6
Bruno Vilalba <brunomgv@gmail.com>
2023-06-20 19:14:21 -0400

    Create slacknotify.sh
    This bash script provides a structured and organized way to send notifications to a Slack channel.

---
+++ slacknotify.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Author: Bruno Vilalba
+# Description: This bash script provides a structured and organized way to send notifications to a Slack channel.
+#
+# Prerequisites: Bash and connection with the internet
+#
+# Configuration: Feel free to customize the message sent
+#
+# Usage: sh slacknotify.sh "Hello, World!"
+#
+# e.g. dev security group: bash aws-update-public-ip dev
+
+# Check if the argument is provided
+if [ $# -eq 0 ]; then
+  echo "Please provide a message as an argument."
+  exit 1
+fi
+
+# Get the message from the command line argument
+message="$1"
+
+# Define the Slack webhook URL
+webhook_url=""
+
+# Send the curl request
+curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"Message: $message\"}" "$webhook_url"
